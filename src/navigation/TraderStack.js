09df6047cfc43ed4f47e15c77d2e1fb1800c2e1b
Nicolas Cianni <nicocianni21@gmail.com>
2024-11-14 12:28:19 -0300

    tradersNav

--- src/navigation/TraderStack.js
+++ src/navigation/TraderStack.js
@@ -1,6 +1,14 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Prapor from "../screens/TradersFolder/Prapor";
+import Therapist from "../screens/TradersFolder/Therapist";
+import Fence from "../screens/TradersFolder/Fence";
+import Skier from "../screens/TradersFolder/Skier";
+import Peacekeeper from "../screens/TradersFolder/Peacekeeper";
+import Ragman from "../screens/TradersFolder/Ragman";
+import Jaeguer from "../screens/TradersFolder/Jaeguer";
+import Ref from "../screens/TradersFolder/Ref";
 import Traders from "../screens/Traders";
+
 
 const Stack = createStackNavigator()
 
@@ -15,6 +23,41 @@ const TraderStack = () => {
             <Stack.Screen name='Prapor' component={Prapor}
             options={{
                 title: "Prapor",
+            }}
+            />
+            <Stack.Screen name='Therapist' component={Therapist}
+            options={{
+                title: "Therapist",
+            }}
+            />
+            <Stack.Screen name='Fence' component={Fence}
+            options={{
+                title: "Fence",
+            }}
+            />
+            <Stack.Screen name='Skier' component={Skier}
+            options={{
+                title: "Skier",
+            }}
+            />
+            <Stack.Screen name='Peacekeeper' component={Peacekeeper}
+            options={{
+                title: "Peacekeeper",
+            }}
+            />
+            <Stack.Screen name='Ragman' component={Ragman}
+            options={{
+                title: "Ragman",
+            }}
+            />
+            <Stack.Screen name='Jaeguer' component={Jaeguer}
+            options={{
+                title: "Jaeguer",
+            }}
+            />
+            <Stack.Screen name='Ref' component={Ref}
+            options={{
+                title: "Ref",
             }}
             />
         </Stack.Navigator>
--- src/screens/Traders.js
+++ src/screens/Traders.js
@@ -11,37 +11,37 @@ const Traders = ({navigation}) => {
 
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Therapist"})}>
                 <ImageBackground source={{uri:"https://th.bing.com/th/id/OIP.2bVykQJSFk-SKO8N-eD1YAHaEK?rs=1&pid=ImgDetMain"}} style={styles.imagenTrader}>
 
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Fence"})}>
                 <ImageBackground source={{uri:"https://th.bing.com/th/id/OIP.WJ1hKz3amg6yXfLQuhevegHaE7?rs=1&pid=ImgDetMain"}} style={styles.imagenTrader}>
 
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Skier"})}>
                 <ImageBackground source={{uri:"https://cdnb.artstation.com/p/assets/images/images/008/629/673/large/ivan-vilmont-2.jpg?1514192145"}} style={styles.imagenTrader}>
                     
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Peacekeeper"})}>
                 <ImageBackground source={{uri:"https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/thumb/d/dd/PeacekeeperFullRes.jpg/1200px-PeacekeeperFullRes.jpg"}} style={styles.imagenTrader}>
                     
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Ragman"})}>
                 <ImageBackground source={{uri:"https://th.bing.com/th/id/OIP.IVaei5VdPgKgXLwliGXV4wHaEK?rs=1&pid=ImgDetMain"}} style={styles.imagenTrader}>
                     
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Jaeguer"})}>
                 <ImageBackground source={{uri:"https://th.bing.com/th/id/R.e56aceaa389204ef36eecefca9e5e2f7?rik=Qn1OhVjbQkHrWg&pid=ImgRaw&r=0"}} style={styles.imagenTrader}>
                     
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.trader}>
+            <TouchableOpacity style={styles.trader} onPress={() => navigation.navigate("TraderStack", {screen: "Ref"})}>
                 <ImageBackground source={{uri:"https://i.ytimg.com/vi/KlgT8IO66-E/maxresdefault.jpg"}} style={styles.imagenTrader}>
                     
                 </ImageBackground>
